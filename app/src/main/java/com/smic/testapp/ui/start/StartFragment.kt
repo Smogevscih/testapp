7ec3a924388f532a3fd2fa503f91dc6c55389fdc
Yuriy <57864435+Smogevscih@users.noreply.github.com>
2021-05-15 11:48:47 +0500

    add onClickListener btnSignInGoogle

--- app/src/main/java/com/smic/testapp/ui/start/StartFragment.kt
+++ app/src/main/java/com/smic/testapp/ui/start/StartFragment.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.smic.testapp.R
 
 
 
 
-class StartFragment : Fragment() {
-
+class StartFragment : Fragment(), View.OnClickListener {
+    private lateinit var btnSignInGoogle: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +23,21 @@ class StartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val root = inflater.inflate(R.layout.fragment_start, container, false)
+        btnSignInGoogle = root.findViewById(R.id.btnSignInGoogle)
 
-        return inflater.inflate(R.layout.fragment_start, container, false)
+
+        btnSignInGoogle.setOnClickListener(this)
+        return root
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnSignInGoogle -> {
+
+            }
+
+        }
     }
 
 
