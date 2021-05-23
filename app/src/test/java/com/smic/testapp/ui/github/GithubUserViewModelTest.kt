435c28363cd52bb004d33b69fcae369f0534161f
Yuriy <57864435+Smogevscih@users.noreply.github.com>
2021-05-23 11:52:59 +0500

    add ModelTest

--- app/src/test/java/com/smic/testapp/ui/github/GithubUserViewModelTest.kt
+++ app/src/test/java/com/smic/testapp/ui/github/GithubUserViewModelTest.kt
@@ -3,39 +3,40 @@ package com.smic.testapp.ui.github
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import com.google.common.truth.Truth
+import com.smic.testapp.model.Model
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class GithubUserViewModelTest {
+class ModelTest {
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
-    lateinit var githubUserViewModel: GithubUserViewModel
+    lateinit var model: Model
 
     @Before
     fun setUp() {
-        githubUserViewModel = GithubUserViewModel()
+        model = Model()
     }
 
     @Test
     fun test_first_launch_adapter_value_is_null() {
-        val result = githubUserViewModel.adapter.value
+        val result = model.getAdapter().value
         Truth.assertThat(result).isNull()
     }
 
     @Test
     fun test_adapter_is_immutable() {
-        val result = githubUserViewModel.adapter
+        val result = model.getAdapter()
         Truth.assertThat(result).isInstanceOf(LiveData::class.java)
     }
 
     @Test
     fun test_first_launch_totalcount_possibleLoad() {
-        val result1 = githubUserViewModel.totalCount
+        val result1 = model.totalCount
         Truth.assertThat(result1).isEqualTo(0)
-        val result2 = githubUserViewModel.possibleLoad
+        val result2 = model.possibleLoad
         Truth.assertThat(result2).isTrue()
     }
 
